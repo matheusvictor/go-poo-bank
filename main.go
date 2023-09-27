06da21db7599e8a955862805070a8d707687b6a4
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-27 12:12:16 -0300

    feat: Cria struct para conta corrente

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import "fmt"
+
+type ContaCorrente struct {
+	titular string
+	agencia int
+	conta   int
+	saldo   float64
+}
+
 func main() {
+	conta := ContaCorrente{
+		titular: "Fulano",
+		agencia: 001,
+		conta:   1234,
+		saldo:   100.50,
+	}
+
+	conta2 := ContaCorrente{
+		"Beltrano",
+		001,
+		5678,
+		100.50,
+	}
+
+	fmt.Println(conta, conta2)
 
 }
